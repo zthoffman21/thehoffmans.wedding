f4949ca33a8612b977b8b76a9d500b7cd3da22f5
zthoffman21 <zachhoffman@ymail.com>
2025-09-19 01:52:51 -0400

    forgot to undo local wrangler.toml

--- .wrangler/tmp/dev-hjwVFz/functionsWorker-0.8336673215973687.js
+++ .wrangler/tmp/dev-hjwVFz/functionsWorker-0.8336673215973687.js
@@ -14435,7 +14435,7 @@ var jsonError2 = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default2 = jsonError2;
 
-// .wrangler/tmp/bundle-00g4DG/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-CQysMB/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__2 = [
   middleware_ensure_req_body_drained_default2,
   middleware_miniflare3_json_error_default2
@@ -14467,7 +14467,7 @@ function __facade_invoke__2(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__2, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-00g4DG/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-CQysMB/middleware-loader.entry.ts
 var __Facade_ScheduledController__2 = class ___Facade_ScheduledController__2 {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
